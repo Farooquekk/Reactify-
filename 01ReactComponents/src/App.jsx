660df3259e7317817b5fea5e200c501a527982d9
Farooquekk <22sw040@students.muet.edu.pk>
2025-01-06 03:42:56 -0700

    Components in React.js

---
+++ 01ReactComponents/src/App.jsx
@@ -0,0 +1,18 @@
+import Header from "./components/Header"
+import Home from "./components/Home"
+
+function App() {
+  
+
+  return (
+    <>
+      {/* <Header></Header> */}
+      {/* short hand notation */}
+      <Header /> 
+      <Home/>
+     
+    </>
+  )
+}
+
+export default App
---
+++ 01ReactComponents/src/components/Header.jsx
@@ -0,0 +1,18 @@
+
+
+function Header() {
+  return (
+      <div>Pikachu
+          <nav>
+              <ul>
+                  <li>Home</li>
+                  <li>About</li>
+                  <li>Contact Us</li>
+                  <li>Pokemon</li>
+              </ul>
+          </nav>
+    </div>
+  )
+}
+
+export default Header
