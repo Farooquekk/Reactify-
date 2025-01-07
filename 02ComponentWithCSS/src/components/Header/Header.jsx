ca09b3d03642321c22c106b833ae46dede0c9fa0
Farooquekk <22sw040@students.muet.edu.pk>
2025-01-07 05:06:48 -0700

    Adding some CSS to React Components.

---
+++ 02ComponentWithCSS/src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import './Header.css'
+
+function Header() {
+    return (
+        <div className="head"><h1>Pikachu</h1>
+            <nav>
+                <ul className="list">
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact Us</li>
+                    <li>Pokemon</li>
+                </ul>
+            </nav>
+      </div>
+    )
+  }
+  
+  export default Header
---
+++ 02ComponentWithCSS/src/components/Home/Home.jsx
@@ -0,0 +1,15 @@
+
+import './Home.css'
+function Home() {
+    return (
+        <div className="home">
+            <img src="https://i.pinimg.com/originals/66/19/07/66190777ab8c5e87596b5f99ecedfbd6.jpg" alt="Pikachu Image"></img>
+  
+            <p>Pikachu is one of the most iconic and beloved characters from the Pokémon franchise. Known for its bright yellow fur, large ears, and adorable appearance, Pikachu is an Electric-type Pokémon that has the ability to generate electricity and release powerful electric shocks. It is the loyal companion of the series main character, Ash Ketchum, and has become a symbol of the Pokémon world, appearing in video games, TV shows, movies, and even merchandise. Pikachu`s cheerful personality and iconic Pika Pika! cry have made it a favorite among fans of all ages.</p>
+  
+  
+      </div>
+    )
+  }
+  
+  export default Home
